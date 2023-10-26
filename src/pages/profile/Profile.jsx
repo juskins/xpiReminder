f77043b7d163b6941966fb936352b9abc663e163
Juskins <omojuwababatunde1@gmail.com>
2023-10-26 13:10:57 +0100

    updated profile page

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -11,32 +11,38 @@ const Profile = () => {
       <div className="newContainer">
         <Navbar />
         <div className="top">
-          <h1>Profile</h1>
+          <h1>My Profile</h1>
         </div>
         <div className="bottom">
-          <div className="left">
-               <img
-               src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-               alt=""
-               className="avatar"
-               />
+          <h2>Edit your profile</h2>
+          <div className="col">
+            <h4>Photo</h4>
+            <div className="right">
+              <img src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+              <input type="file" name="" id="" />
+            </div>
           </div>
-          <div className="right">
-            <div className="col">
-               <h3>UserName</h3>
-               <p>Omojuwa Babatunde</p>
-            </div>
-            
-            <div className="col">
-               <h3>Email Address</h3>
-               <p>omojuwababatunde1@gmail.com</p>
-            </div>
 
-            <div className="col">
-               <h3>Business Name</h3>
-               <p>Omojuwa Stores</p>
-            </div>
+          <div className="col">
+            <h4>Business Name</h4>
+            <input type="text" placeholder='JJ Stores'/>
           </div>
+
+          <div className="col">
+            <h4>Email</h4>
+            <input type="email" placeholder='omojuwababatunde1@gmail.com' readOnly/>
+          </div>
+
+          <div className="col">
+            <h4>Phone Number</h4>
+            <input type="tel" placeholder='+234-70-810-51-810'/>
+          </div>
+
+          <div className="col">
+            <h4>Address</h4>
+            <input type="tel" placeholder='35, church avenue Kosofe Mile 12'/>
+          </div>
+          <button>Save Information</button>
         </div>
       </div>
     </div>
