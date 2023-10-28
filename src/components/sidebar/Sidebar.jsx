3ba8a25e46eb1ec25db28c2eb0b5e2613d4daa2d
fktona <adetona.fk@gmail.com>
2023-10-27 22:15:47 -1100

    backend

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -14,7 +14,7 @@ import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined
 import { Link } from "react-router-dom";
 import { DarkModeContext } from "../../context/darkModeContext";
 import { useContext } from "react";
-//import logo from "../../assets/logo.svg"
+import logo from "../../assets/logo.svg"
 
 
 const Sidebar = () => {
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,6 @@
 import "./login.scss";
 import loginImage from "../../assets/supermarket.jpg"
-//import googleImg from "../../assets/icons8-google.svg"
+import googleImg from "../../assets/icons8-google.svg"
 import { Link } from "react-router-dom"
 //import logo from "../../assets/logo.svg"
 import React , {useState} from 'react';
