b2a6d13b1b3b523ae76612e751e4691c1b94c272
cybertronbot <okhuomonajayi54@gmail.com>
2023-11-08 08:31:20 +0100

    login

--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import './index.css';
 import { DarkModeContextProvider } from "./context/darkModeContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
+
 import { AuthProvider } from "./context/AuthProvider";
+
 ReactDOM.render(
   <React.StrictMode>
+   
     <DarkModeContextProvider>
-    <AuthProvider>
-      <App />
+      
+      <AuthProvider>
+      <App/>
+     
     </AuthProvider>
+   
     </DarkModeContextProvider>
+    
   </React.StrictMode>,
   document.getElementById("root")
 );
